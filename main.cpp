97b9299f0d920d8e6f631e9c6795e242e7f7f1da
holzm <54302524+holzm@users.noreply.github.com>
2020-03-14 19:22:02 +0800

    main.cpp注释

--- main.cpp
+++ main.cpp
@@ -14,11 +14,13 @@ int main()
 	//VideoCapture cap0("D:/文档/visual studio/视觉组/装甲识别/装甲板识别测试视频（蓝色）.mp4");
 	Mat frame0;//用于储存每一帧的图像
 	ArmorParam armorParam;
-	ArmorDetector armor(armorParam);
+	ArmorDetector armor(armorParam);//initialize param @armor
 	Pose pose;
 	Point2d offset;
-	armor.setEnemyColor(RED);
+
+	armor.setEnemyColor(RED);//set the color of the enemy's armor as RED
 	namedWindow("1", WINDOW_AUTOSIZE);
+
 	while (1)
 	{
 
@@ -27,12 +29,12 @@ int main()
 			cap0 >> frame0;
 			armor.loadImg(frame0);
 			if (armor.detect() == 3) {
-				receive(fdk_f);
-				offset = pose.getArmorPoint(armor,fdk_f.bulletType);
-				ctl_f.pitch = offset.x;
-				ctl_f.yaw = offset.y;
-				send(ctl_f);
-				cout << "offset: " << offset << endl;//
+				receive(fdk_f);//receive feedback frame
+				offset = pose.getArmorPoint(armor,fdk_f.bulletType);//get angle offset
+				ctl_f.pitch = offset.x;//set control pitch in x-asxis
+				ctl_f.yaw = offset.y;//set control yaw in y-axis
+				send(ctl_f);//send control frame
+				cout << "offset: " << offset << endl;//output angle offset
 				
 			}
 			armor.showDebugImg();
@@ -46,4 +48,6 @@ int main()
 	return 0;
 
 }
+
+
 
